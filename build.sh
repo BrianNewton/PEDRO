96f1e1f88c655886698485810f479c03a989bf88
BrianNewton <brian.newton@uwaterloo.ca>
2022-02-25 11:05:37 -0500

    Build script

---
+++ build.sh
@@ -0,0 +1,1 @@
+pyinstaller --onedir --add-binary "/Users/briannewton/OneDrive - University of Waterloo/Wetlands lab/Code/.peat-intel/lib/python3.9/site-packages/tabula/tabula-1.0.5-jar-with-dependencies.jar:./tabula/" PEDRO.py
