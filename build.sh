1c31db205c2231aeaa7ec5bb6baf163a52571954
BrianNewton <brian.newton@uwaterloo.ca>
2022-04-27 11:02:26 -0400

    removing build.sh from repo

--- build.sh
+++
@@ -1,2 +0,0 @@
-rm -rf dist/ build/
-pyinstaller --onedir --add-binary "/Users/briannewton/OneDrive - University of Waterloo/Wetlands lab/Code/.peat-intel/lib/python3.9/site-packages/tabula/tabula-1.0.5-jar-with-dependencies.jar:./tabula/" PEDRO.py
