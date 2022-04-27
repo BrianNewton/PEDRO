fb9d0ab5695dccc3f39550a46172ef9c9ebc6823
BrianNewton <brian.newton@uwaterloo.ca>
2022-04-27 11:26:19 -0400

    removing unstable message from GC, (hopefully true)

--- PEDRO.py
+++ PEDRO.py
@@ -23,7 +23,7 @@ def error_screen(e):
 layout = [[sg.Text("P.E.D.R.O.", font='Any 48', background_color="#DF4A4A")],
     [sg.Text("\"Peatland Equipment Data Re-Organizer\"", font ='italic', background_color="#DF4A4A")],
     [sg.Text(background_color="#DF4A4A")],
-    [sg.Text("Select an option bellow:", background_color="#DF4A4A")], [sg.Button("FMA"), sg.Button("LICOR"), sg.Button("GC (unstable)"), sg.Button("IRGA (coming soon!)")]]
+    [sg.Text("Select an option bellow:", background_color="#DF4A4A")], [sg.Button("FMA"), sg.Button("LICOR"), sg.Button("GC"), sg.Button("IRGA (coming soon!)")]]
 
 # Create the window
 window = sg.Window("PEDRO", layout, margins=(60, 20), background_color="#DF4A4A")
